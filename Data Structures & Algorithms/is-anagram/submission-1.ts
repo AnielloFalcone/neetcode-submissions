ed38dd244bda49427685f7831d75a31685c5613a
Aniello Falcone <aniellofalcone92@gmail.com>
2026-07-24 12:40:44 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+ isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+
+    const count = new Int8Array(26);
+    for (let i = 0; i < s.length; i++) {
+        count[s.charCodeAt(i) - 97]++;
+        count[t.charCodeAt(i) - 97]--;
+    }
+    for (let i = 0; i < 26; i++) {
+        if (count[i] !== 0) return false;
+    }
+    return true;
+}
+}
