0c231665f45cab958938a25169fa9333aa366b72
Aniello Falcone <aniellofalcone92@gmail.com>
2026-07-28 14:34:26 +0200

    Add: anagram-groups - submission-17

---
+++ Data Structures & Algorithms/anagram-groups/submission-17.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+    const groups = new Map<string, string[]>();
+    for (const word of strs) {
+        const count = new Array(26).fill(0);
+        for (const ch of word) count[ch.charCodeAt(0) - 97]++;
+        const key = count.join('#'); // es: "2#0#1#0#..."
+
+        if (!groups.has(key)) groups.set(key, []);
+        groups.get(key)!.push(word);
+    }
+    return Array.from(groups.values());
+}
+}
